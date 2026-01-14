030ac703776437b3350ec7084fb1285848415f6f
Carlos Freitas <carlosfreitassdev@gmail.com>
2026-01-14 16:44:06 -0300

    DAO com Promises

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,12 +4,16 @@ class LivroDao {
     this._db = db
   }
 
-  lista(callback) {
-    this._db.all(
-      'SELECT * from livros',
-      (erro, resultados) =>
-        callback(erro, resultados)
-    )
+  lista() {
+    return new Promise((resolve, reject) => {
+      this._db.all(
+        'SELECT * from livros',
+        (erro, resultados) => {
+          if (erro) return reject('Não foi possível listar os livros!')
+          return resolve(resultados)
+        }
+      )
+    })
   }
 }
 
