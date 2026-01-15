47fd8eece30835722433eca91b54aca3cb2d923f
Carlos Freitas <carlosfreitassdev@gmail.com>
2026-01-15 09:35:15 -0300

    Arquivos estáticos + Método remove

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -40,6 +40,22 @@ class LivroDao {
       )
     })
   }
+
+  remove(id) {
+    return new Promise((resolve, reject) => {
+      this._db.run(
+        'DELETE FROM livros WHERE id = ?',
+        [id],
+        function (err) {
+          if (err) {
+            return reject('Não foi possível remover o livro!')
+          }
+
+          resolve()
+        }
+      )
+    })
+  }
 }
 
 module.exports = LivroDao
